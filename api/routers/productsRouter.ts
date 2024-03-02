07ecf7960b6147c2625ff9b4c84e7e060c97221f
Mamanov Emil <emil.mamanov97@gmail.com>
2024-03-02 19:10:36 +0600

    added products router

---
+++ api/routers/productsRouter.ts
@@ -0,0 +1,53 @@
+import {Router} from "express";
+import Product from "../models/Product";
+import auth, { RequestWithUser } from "../auth";
+import {uploadItemImage} from "../multer";
+import mongoose from "mongoose";
+
+
+const productsRouter = Router();
+
+productsRouter.post(
+    '/',
+    auth,
+    uploadItemImage.single('image'),
+    async (req: RequestWithUser, res, next) => {
+    try {
+
+        const productData  = {
+            user: req.user?._id,
+            title: req.body.title,
+            description: req.body.description,
+            price: parseFloat(req.body.price),
+            image: req.file ? req.file.filename : null,
+            category: req.body.category,
+        };
+
+        const product = new Product(productData);
+
+        await product.save();
+
+        return res.send(product);
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(422).send(e);
+        }
+
+        next(e);
+    }
+});
+
+productsRouter.get('/', async (_req, res, next) => {
+    try {
+        const products = await Product.find();
+
+        return res.send(products);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+
+
+
+export default productsRouter;
